a8090598a34456e6c7e29eabe183ee815ca702ae
Samiuddin <mdsamiuddin2005@outlook.com>
2025-04-07 19:57:34 +0530

    add testing async await in next app

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,13 @@
+import axios from 'axios';
 import React from 'react';
 
-export default function Home() {
+export  default async function Home() {
+  const data = await axios.get("https://dog.ceo/api/breeds/image/random")
   return (
     <React.Fragment>
       <div>
         Hello everyone, I hope you are fine
+        <img src={data.data.message} alt="random dog image" />
       </div>
     </React.Fragment>
   );
